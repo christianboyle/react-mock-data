0eea581dd08da570ca7e90ca5995a0ab9a07e8a5
Christian Boyle <mail@christianboyle.com>
2021-12-18 08:14:20 -0600

    add handleEdit

--- src/App.js
+++ src/App.js
@@ -1,67 +1,82 @@
-import React from 'react';
+import React from 'react'
 
-import { getUsers, createUser, updateUser, deleteUser } from './api';
+import { getUsers, createUser, updateUser, deleteUser } from './api'
 
 const getDeveloperText = (isDeveloper) =>
-  `is ${isDeveloper ? 'a' : 'not a'} developer`;
+  `is ${isDeveloper ? 'a' : 'not a'} developer`
 
 const App = () => {
-  const [users, setUsers] = React.useState(null);
+  const [users, setUsers] = React.useState(null)
 
   const doGetUsers = React.useCallback(async () => {
     try {
-      const result = await getUsers();
-      setUsers(result);
+      const result = await getUsers()
+      setUsers(result)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  }, []);
+  }, [])
 
   React.useEffect(() => {
-    doGetUsers();
-  }, [doGetUsers]);
+    doGetUsers()
+  }, [doGetUsers])
 
   const refetchUsers = async () => {
-    await doGetUsers();
-  };
+    await doGetUsers()
+  }
 
-  const [firstName, setFirstName] = React.useState('');
-  const [lastName, setLastName] = React.useState('');
+  const [firstName, setFirstName] = React.useState('')
+  const [lastName, setLastName] = React.useState('')
 
   const handleChangeFirstName = (event) => {
-    setFirstName(event.target.value);
-  };
+    setFirstName(event.target.value)
+  }
 
   const handleChangeLastName = (event) => {
-    setLastName(event.target.value);
-  };
+    setLastName(event.target.value)
+  }
 
   const handleCreate = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     try {
-      await createUser({ firstName, lastName, isDeveloper: false });
-      await refetchUsers();
+      await createUser({ firstName, lastName, isDeveloper: false })
+      await refetchUsers()
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
+
+  const handleEdit = async (id) => {
+    const user = users.find((user) => user.id === id)
+    const isDeveloper = !user.isDeveloper
+
+    try {
+      await updateUser(id, { isDeveloper })
+      await refetchUsers()
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   if (!users) {
-    return null;
+    return null
   }
 
   return (
     <div>
       <ul>
         {users.map((user) => {
-          const developerText = getDeveloperText(user.isDeveloper);
+          const developerText = getDeveloperText(user.isDeveloper)
 
           return (
             <li key={user.id}>
               {user.firstName} {user.lastName} {developerText}
+              <button type='button' onClick={() => handleEdit(user.id)}>
+                Toggle Developer (Update)
+              </button>
             </li>
-          );
+          )
         })}
       </ul>
 
@@ -72,18 +87,18 @@ const App = () => {
       <form onSubmit={handleCreate}>
         <label>
           First Name:
-          <input type="input" onChange={handleChangeFirstName} />
+          <input type='input' onChange={handleChangeFirstName} />
         </label>
 
         <label>
           Last Name:
-          <input type="input" onChange={handleChangeLastName} />
+          <input type='input' onChange={handleChangeLastName} />
         </label>
 
-        <button type="submit">Create</button>
+        <button type='submit'>Create</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
